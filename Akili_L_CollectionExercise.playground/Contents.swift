f707943bfdae4ae7ee30ac6883149db428bcde73
London2021 <67976260+London2021@users.noreply.github.com>
2020-07-15 14:26:46 -0500

    Collection Exercise Commit Changes

---
+++ Akili_L_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+
+var wigs: [String] = ["12 inch platnuim blonde middle-part bob closure wig"]
+    wigs += ["16 inch side-part illusion closure wig", "20 inch honey blonde middle-part illusion closure wig", "20 inch middle-part closure wig", "24 inch middle-part closure wig", "26 inch middle-part illusion closure wig", "26 inch side-part  illusion closure wig with higlights", "26 inch middle-part closure wig"]
+for items in wigs { print (items)}
+print("The wig list contains \(wigs.count) items.")
